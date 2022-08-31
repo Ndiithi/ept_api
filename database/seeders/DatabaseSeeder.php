0a0c5b4de6fbc99a81931f15d2db114d6b8c1af2
ndiithi <duncanndiithi@yahoo.com>
2022-08-31 19:47:54 +0300

    add models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace Database\Seeders;
+namespace App\Traits;
 
+use Illuminate\Support\Str;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -20,5 +22,21 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        
+
+    // "program": {
+    //     "uuid": "uuid",
+    //     "name": "string",
+    //     "description": "string",
+    //     "meta": "json" //e.g. active_theme
+    // },
+        $data = array(
+            array('id' => 1, 'name' => 'super admin', 'editor_id' => 1, 'created_at' => new \dateTime, 'updated_at' => new \dateTime),
+        );
+        
+        $authObj = new Role();
+        Role::query()->truncate();
+        $authObj->insert($data);
+        // $authObj->save();
     }
 }
