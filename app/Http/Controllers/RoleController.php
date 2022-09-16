7461103ac36d706ec786412442cbae54765ec7f6
ndiithi <duncanndiithi@yahoo.com>
2022-09-16 09:54:01 +0300

    roles controller

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
+use App\Services\SystemAuthorities;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class RoleController extends Controller
 {
-    //
+
+    public function updateRole()
+    {
+
+        if (!Gate::allows(SystemAuthorities::$authorities['edit_role'])) {
+            return response()->json(['Message' => 'Not allowed to edit roles: '], 401);
+        } else {
+            return response()->json(['Message' => 'Success '], 200);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UITemplateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,7 +22,11 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('/getUITemplates', [UITemplateController::class, 'getUITemplate'])->middleware('auth:sanctum');;
+Route::get('/getUITemplates', [UITemplateController::class, 'getUITemplate'])->middleware('auth:sanctum');
 
 Route::post('/auth/register', [AuthController::class, 'createUser'])->name('register');;
 Route::post('/auth/login', [AuthController::class, 'loginUser'])->name('login');
+
+// viewAny, view, create, update, delete, restore, and forceDelete
+Route::post('/role/update', [RoleController::class, 'updateRole'])->middleware('auth:sanctum');
+
