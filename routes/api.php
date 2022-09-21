a566c152b52ba87f45ea1fd92670a92a5bf7b7d4
ndiithi <duncanndiithi@yahoo.com>
2022-09-21 23:10:42 +0300

    add path parameters for json schmema querying

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('/getUITemplates', [UITemplateController::class, 'getUITemplate'])->middleware('auth:sanctum');
+Route::get('/getUITemplates/{scheme}/{schema_id}', [UITemplateController::class, 'getUITemplate'])->middleware('auth:sanctum');
 
 Route::post('/auth/register', [AuthController::class, 'createUser'])->name('register');;
 Route::post('/auth/login', [AuthController::class, 'loginUser'])->name('login');
